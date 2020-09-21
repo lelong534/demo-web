afef8edf04c1b4b87bb9697af271b39690b78fef
lelong534 <lelong220798@gmail.com>
2020-09-21 17:26:14 +0700

    update css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -76,6 +76,7 @@
                                 <h2 class="title">Dịch vụ di động</h2>
                                 <p class="is-desktop">Gói cước</p>
                                 <p class="is-desktop">Gói data</p>
+                                <a href="#" class="all is-desktop">Xem tất cả<span> ></span></a>
                             </div>
                             <div class="content-body"> 
                                 <div class="m-card mobile-service">
@@ -133,8 +134,27 @@
                         <div class="service-content">
                             <div class="content-header">
                                 <h2 class="title">Dịch vụ</h2>
+                                <a href="#" class="all is-desktop">Xem tất cả<span> ></span></a>
                             </div>
                             <div class="content-body"> 
+                                <div class="m-card service">
+                                    <div class="content-image">
+                                        <img src="{{asset('source/icon_mobile/data_d5.png')}}">
+                                    </div>
+                                    <h1 class="title">Gói Data ngày D5</h1>
+                                    <div class="m-card-body">
+                                        Với gói cước D5 bạn sẽ có 1 GB tốc độ cao/ngày và trong suốt 30 ngày, bạn sẽ không còn nỗi lo thiếu data để làm việc, giải trí…
+                                    </div>
+                                </div>
+                                <div class="m-card service">
+                                    <div class="content-image">
+                                        <img src="{{asset('source/icon_mobile/data_d5.png')}}">
+                                    </div>
+                                    <h1 class="title">Gói Data ngày D5</h1>
+                                    <div class="m-card-body">
+                                        Với gói cước D5 bạn sẽ có 1 GB tốc độ cao/ngày và trong suốt 30 ngày, bạn sẽ không còn nỗi lo thiếu data để làm việc, giải trí…
+                                    </div>
+                                </div>
                                 <div class="m-card service">
                                     <div class="content-image">
                                         <img src="{{asset('source/icon_mobile/data_d5.png')}}">
@@ -153,7 +173,10 @@
                         <!-- NEWS AND EVENTS -->
                         <div class="events-content">
                             <div class="content-header">
-                                <h2 class="title">Dịch vụ</h2>
+                                <h2 class="title">Tin tức sự kiện</h2>
+                                <p class="is-desktop">Tin sự kiện</p>
+                                <p class="is-desktop">Tin khuyến mãi</p>
+                                <a href="#" class="all is-desktop">Xem tất cả<span> ></span></a>
                             </div>
                             <div class="content-body">
                                 <div class="m-card events"> 
@@ -264,18 +287,69 @@
                     </div>
                 </div>
             </main>
-            <footer>
-                <div class="footer-row">   
+            <div class="footer">
+                <div class="inner">
+                <div class="footer-row is-desktop">
+                    <div class="footer-column-first">
+                        <div class="column-item-first">Giới thiệu</div>
+                        <div class="column-item"><a href="#">Giới thiệu Mobifone</a></div>
+                        <div class="column-item"><a href="#">Hợp tác Mobifone</a></div>
+                        <div class="column-item"><a href="#">Tuyển dụng</a></div>
+                        <div class="column-item"><a href="#">Điểm cung cấp dịch vụ viễn thông</a></div>
+                        <div class="column-item"><a href="#">Site map</a></div>
+                        <div class="column-item"><a href="#">Bảo mật thông tin</a></div>
+                        <div class="social-media">
+                            <img src="{{asset('source/icons/facebook.svg')}}" class="icon" alt="icons facebook" />
+                            <img src="{{asset('source/icons/youtube-square.svg')}}" class="icon" alt="youtube square" />
+                            <img src="{{asset('source/icons/news.svg')}}" class="icon" alt="icons news" />
+                        </div>
+                    </div>
+                    <div class="footer-column">
+                        <div class="column-item-first">Dịch vụ di động</div>
+                        <div class="column-item"><a href="#">Gói cước</a></div>
+                        <div class="column-item"><a href="#">Gói data</a></div>
+                        <div class="column-item"><a href="#">Dịch vụ</a></div>
+
+                        <div class="column-item-first">Tin tức</div>
+                        <div class="column-item"><a href="#">Chương trình khuyến mãi</a></div>
+                        <div class="column-item"><a href="#">Tin tức sự kiện</a></div>
+                        <div class="column-item"><a href="#">Thông cáo báo chí</a></div>
+                        <div class="column-item"><a href="#">Tuyển dụng</a></div>
+                    </div>
+                    <div class="footer-column">
+                        <div class="column-item-first">Hỗ trợ khách hàng</div>
+                        <div class="column-item"><a href="#">Gửi phản ánh</a></div>
+                        <div class="column-item"><a href="#">Câu hỏi thường gặp</a></div>
+                        <div class="column-item"><a href="#">Tìm kiếm cửa hàng</a></div>
+                        <div class="column-item"><a href="#">Chọn số Mobifone</a></div>
+                        <div class="column-item"><a href="#">Chuyển mạng giữ số</a></div>
+                        <div class="column-item-first">Tài khoản</div>
+                        <div class="column-item"><a href="#">Thông tin tài khoản</a></div>
+                        <div class="column-item"><a href="#">Tra cước</a></div>
+                    </div>
+                    <div class="footer-column">
+                        <div class="column-item-first">Tiện ích</div>
+                        <div class="column-item"><a href="#">Nạp tiền</a></div>
+                        <div class="column-item"><a href="#">Chuyển tiền</a></div>
+                        <div class="column-item"><a href="#">Thanh toán ngân hàng</a></div>
+                        <div class="column-item-first">Kết nối dài lâu</div>
+                        <div class="column-item"><a href="#">Thông tin hội viên</a></div>
+                        <div class="column-item"><a href="#">Danh sách ưu đãi</a></div>
+                        <div class="column-item"><a href="#">Đổi điểm</a></div>
+                    </div>
+                </div>
+                <div class="footer-row-mobile is-mobile">
+                <div class="footer-row right">   
                     <div class="footer-img">
                         <img src="{{asset('source/icon_mobile/logo-last.png')}}" alt="logo">
                     </div>
-                    <p class="sign">Giấy chứng nhận đăng ký doanh nghiệp. mã số doanh nghiệp: 0100686209, Đăng ký thay đổi lần thứ 9 ngày 27/7/2018, cấp bởi sở KHĐT thành phố Hà Nội.</p> 
-                    <div class="social-media">
+                    <p class="sign">Giấy chứng nhận đăng ký doanh nghiệp. mã số doanh nghiệp: 0100686209, Đăng ký thay đổi lần thứ 9 ngày 27/7/2018,<br class="is-desktop" /> cấp bởi sở KHĐT thành phố Hà Nội.</p> 
+                    <div class="social-media is-mobile">
                         <img src="{{asset('source/icons/facebook.svg')}}" class="icon" alt="icons facebook" />
                         <img src="{{asset('source/icons/youtube-square.svg')}}" class="icon" alt="youtube square" />
                         <img src="{{asset('source/icons/news.svg')}}" class="icon" alt="icons news" />
                     </div>   
-                    <div class="line"></div>        
+                    <div class="line is-mobile"></div>        
                 </div>    
                 <div class="footer-row">
                     <div class="info">
@@ -291,7 +365,9 @@
                         <span>(+84-24)37831734</span>
                     </div>
                 </div>
-            </footer>
+            </div>
+            </div>
+            </div>
             <div class="footer-last">
                 © Copyright  2020 - Mobifone.
             </div>
